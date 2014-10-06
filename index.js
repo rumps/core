200992289e3bcafe308ed8a6830245c04b1c1914
jupl <juan.placencia.512@gmail.com>
2014-10-06 11:48:16 -0500

    Expose main config and add event emitter for others to take advantage of adding custom configurations

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 'use strict';
 
+var events = require('events');
 var configs = require('./configs');
-
-var rump = module.exports = function(options) {
-  configs.rebuild(options);
-  return rump;
-};
+var rump = module.exports = new events.EventEmitter();
 
 rump.addGulpTasks = function() {
   require('./gulp');
 };
 
+rump.reconfigure = function(options) {
+  configs.rebuild(options);
+  rump.emit('update', rump.configs);
+};
+
 rump.configs = {
+  get main() {
+    return configs.main;
+  },
   get webpack() {
     return configs.webpack;
   },

13eece4814f55aebeee61aafc031c32f9943817e
jupl <juan.placencia.512@gmail.com>
2014-10-07 10:08:29 -0500

    Add global globs that can be used for global ignores

--- configs/index.js
+++ configs/index.js
@@ -5,6 +5,9 @@ var merge = require('merge');
 exports.rebuild = function(overrides) {
   exports.main = merge.recursive({
     environment: (process.env.NODE_ENV || 'development').trim(),
+    globs: {
+      global: []
+    },
     paths: {
       source: {
         root: 'src'

eba06610a4c30450631a0c272da057c74cb04ad0
jupl <juan.placencia.512@gmail.com>
2014-10-09 22:04:25 -0500

    Use extend lib

--- configs/index.js
+++ configs/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var merge = require('merge');
+var extend = require('extend');
 
 exports.rebuild = function(overrides) {
-  exports.main = merge.recursive({
+  exports.main = extend(true, {
     environment: (process.env.NODE_ENV || 'development').trim().toLowerCase(),
     globs: {
       global: []

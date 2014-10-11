66c124a9cf18b9504076c6d799425d771ad432f2
jupl <juan.placencia.512@gmail.com>
2014-10-11 18:31:09 -0500

    Don't depend on gulp-util for info task

--- gulp/info.js
+++ gulp/info.js
@@ -1,11 +1,13 @@
 'use strict';
 
+var chalk = require('chalk');
 var gulp = require('gulp');
-var util = require('gulp-util');
 var configs = require('../configs');
 
 gulp.task('rump:info:core', function() {
-  util.log('Environment:', util.colors.green(configs.main.environment));
+  console.log(chalk.magenta('--- Core'));
+  console.log('Environment is', chalk.green(configs.main.environment));
+  console.log();
 });
 
 gulp.task('rump:info', ['rump:info:core']);

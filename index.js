55c823e3fd29a9ed4eee7bac24fec69f518c5edf
jupl <juan.placencia.512@gmail.com>
2014-10-12 20:07:27 -0500

    Add ability to automatically load available modules defined

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 'use strict';
 
 var events = require('events');
+var path = require('path');
 var configs = require('./configs');
 var rump = module.exports = new events.EventEmitter();
+
+rump.autoload = function() {
+  var pkg = require(path.resolve('package.json'));
+  var modules = [].concat(Object.keys(pkg.dependencies || {}),
+                          Object.keys(pkg.devDependencies || {}),
+                          Object.keys(pkg.peerDependencies || {}));
+
+  modules.forEach(function(mod) {
+    try { require(mod); } catch(e) {}
+  });
+};
 
 rump.addGulpTasks = function() {
   require('./gulp');

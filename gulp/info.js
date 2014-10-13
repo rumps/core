537615c2fb0e9ac236f933385452d3c84e2baeac
jupl <juan.placencia.512@gmail.com>
2014-10-13 09:29:38 -0500

    Add version to info task

--- gulp/info.js
+++ gulp/info.js
@@ -3,10 +3,11 @@
 var chalk = require('chalk');
 var gulp = require('gulp');
 var configs = require('../configs');
+var pkg = require('../package');
 
 gulp.task('rump:info:core', function() {
   console.log();
-  console.log(chalk.magenta('--- Core'));
+  console.log(chalk.magenta('--- Core', 'v' + pkg.version));
   console.log('Environment is', chalk.green(configs.main.environment));
   console.log();
 });
